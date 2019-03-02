8f1ae160650431af47060c736db9c1dd93cf7aa0
mkufel <m.kufel@hotmail.com>
2019-03-02 04:54:12 +0100

    Transitions

---
+++ app/src/main/java/com/example/whatthehack/Haseeb.java
@@ -0,0 +1,13 @@
+package com.example.whatthehack;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Haseeb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
--- app/src/main/java/com/example/whatthehack/MainActivity.java
+++ app/src/main/java/com/example/whatthehack/MainActivity.java
@@ -1,13 +1,99 @@
 package com.example.whatthehack;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    ArrayList<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView list = (ListView) findViewById(R.id.theList);
+        EditText theFilter = (EditText) findViewById(R.id.searchFilter);
+
+        names = new ArrayList<>();
+        names.add("Mitch");
+        names.add("Blake");
+        names.add("Shelly");
+        names.add("Jess");
+        names.add("Steve");
+        names.add("Mohammed");
+
+        adapter = new ArrayAdapter(this, R.layout.list_item_layout, names);
+        list.setAdapter(adapter);
+
+        theFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                (MainActivity.this).adapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3)
+            {
+                Intent intent = new Intent(MainActivity.this, Haseeb.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        System.out.println("Item selected");
+        if (item.getTitle().toString().equals("Saved")) {
+            item.setTitle("Back to feed");
+            names.clear();
+            names.add("Saved drug");
+            names.add("Saved drug2");
+        } else {
+            item.setTitle("Saved");
+            names.clear();
+            names.add("New drug 1");
+            names.add("New drug 2");
+        }
+
+        adapter.notifyDataSetChanged();
+        return super.onOptionsItemSelected(item);
     }
 }
